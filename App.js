cf2d6bf769b52352e91542aecbadcb6788ca7278
Ishan Arefin <ashiqul.arefin@stonybrook.edu>
2024-03-31 13:22:54 -0400

    Async Storage set up in a way that can handle if it's a user's first time launch. Additionally, it will not reset global counter if the user has launched before

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Navigation Imports
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -8,56 +9,51 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 // Screens
 import Home from './src/screens/Home';
 
+// Async Storage Import 
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const listAllKeys = async () => {
-  try {
-    const keys = await AsyncStorage.getAllKeys();
-    console.log('All keys in AsyncStorage:', keys);
-  } catch (e) {
-    console.log('Error listing all keys:', e);
-  }
-};
+// This function lists all of the keys in AsyncStorage... For Development Use Only
+// const listAllKeys = async () => {
+//   try {
+//     const keys = await AsyncStorage.getAllKeys();
+//     console.log('All keys in AsyncStorage:', keys);
+//   } catch (e) {
+//     console.log('Error listing all keys:', e);
+//   }
+// };
+// listAllKeys();
 
-listAllKeys();
+//If this is the first launch then global counter is not set up in async storage, create global counter if this is the first launch
+const initializeCounter = async () => {
+	try {
+		const counter = await AsyncStorage.getItem('globalItemCounter');
 
-const getData = async (item) => {
-  try {
-    const jsonValue = await AsyncStorage.getItem(item);
-    console.log(JSON.parse(jsonValue))
-    return jsonValue != null ? JSON.parse(jsonValue) : null;
-  } catch (e) {
-    // error reading value
-  }
-};
+		// Create the counter if it doesn't exist. Default value is zero. 
+		if (counter === null) {
+			await AsyncStorage.setItem('globalItemCounter', JSON.stringify({ itemCount: 0 }));
+			console.log("Global counter initialized to 0")
+		} else {
+			console.log('Global counter exists with value of: ' + JSON.parse(counter).itemCount)
+		}
+	} catch (error) {
+		console.error('Error initializing the counter', error)
+	}
+}
 
-getData("Item1")
-
-// Code below is initializing database with globalCount
-// AsyncStorage.setItem('globalItemCount', JSON.stringify({itemCount: 0}))
-
-// Code to get the global count
-// globalCount = 0 
-// AsyncStorage.getItem('globalItemCount')
-// .then(value => {
-//     globalCount = JSON.parse(value).count
-// })
-// .catch(error => console.error('Error retrieving count:', error))
-// console.log(globalCount)
-
+initializeCounter();
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-  const Drawer = createDrawerNavigator();
-  
+	const Stack = createNativeStackNavigator();
+	const Drawer = createDrawerNavigator();
 
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName='Home' style={{backgroundColor: 'black'}}>
-        <Drawer.Screen name="Food Tracker" component={Home}/>
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator initialRouteName='Home' style={{ backgroundColor: 'black' }}>
+				<Drawer.Screen name="Food Tracker" component={Home} />
+			</Drawer.Navigator>
+		</NavigationContainer>
+	);
 }
 
 
